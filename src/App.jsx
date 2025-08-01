00a787ddc05d9a0ae8afde436cb569968a9cef7a
milad vlp <you@example.com>
2025-08-01 11:25:04 +0200

    remove navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
 import Features from './components/Features'
 import Story from './components/Story'
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero/> 
       <About />
       <Features />
